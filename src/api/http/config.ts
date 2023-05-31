6700bed2d0acac2868cab92a649d82ef05b1febb
eden <maoshencat@gmail.com>
2023-05-31 16:07:27 +0800

    config

--- src/api/http/config.ts
+++ src/api/http/config.ts
@@ -1,17 +1,17 @@
 export const CURRENT_CONFIG = {
 
   // license
-  appId: 'Please enter the app id.',  // You need to go to the development website to apply.
-  appKey: 'Please enter the app key.',  // You need to go to the development website to apply.
-  appLicense: 'Please enter the app license.', // You need to go to the development website to apply.
-  
+  appId: '127669', // You need to go to the development website to apply.
+  appKey: '6bc87f55c8882f4c308659ee398ef32', // You need to go to the development website to apply.
+  appLicense: 'p2ipNLgpZMgewqyeo14Zc+gDqwCtaEcIkv3k76BLFrYxf6uT2ztHIHA3ude+yzqm3LKQdOSWGqGyynxxYGpQtqj4JRqV6mTUFab8hBgcRqCW5hupzZextcenRvafT9hox8vl7gIznqnRJBsktVCNYR99oJRkE9ysvaD4ASXgCX0=', // You need to go to the development website to apply.
+
   // http
-  baseURL: 'Please enter the backend access address prefix.', // This url must end with "/". Example: 'http://192.168.1.1:6789/'
-  websocketURL: 'Please enter the WebSocket access address.',  // Example: 'ws://192.168.1.1:6789/api/v1/ws'
+  baseURL: 'http://43.143.34.8:6789/', // This url must end with "/". Example: 'http://192.168.1.1:6789/'
+  websocketURL: 'ws://43.143.34.8:6789/api/v1/ws', // Example: 'ws://192.168.1.1:6789/api/v1/ws'
 
   // livestreaming
   // RTMP  Note: This IP is the address of the streaming server. If you want to see livestream on web page, you need to convert the RTMP stream to WebRTC stream.
-  rtmpURL: 'Please enter the rtmp access address.',  // Example: 'rtmp://192.168.1.1/live/' 
+  rtmpURL: 'rtmp://43.143.34.8/live/', // Example: 'rtmp://192.168.1.1/live/'
   // GB28181 Note:If you don't know what these parameters mean, you can go to Pilot2 and select the GB28181 page in the cloud platform. Where the parameters same as these parameters.
   gbServerIp: 'Please enter the server ip.',
   gbServerPort: 'Please enter the server port.',
@@ -25,12 +25,12 @@ export const CURRENT_CONFIG = {
   rtspPassword: 'Please enter the password.',
   rtspPort: '8554',
   // Agora
-  agoraAPPID: 'Please enter the agora app id.',
-  agoraToken: 'Please enter the agora temporary token.',
-  agoraChannel: 'Please enter the agora channel.',
+  agoraAPPID: '222fff02ae6644c1bb37dcd2e9cfa964',
+  agoraToken: '007eJxTYHjPf/DMteRnc98zb7t+lmd1Cfdk3f2SW8qa+vPbJvEqn3muwGBkZJSWlmZglJhqZmZikmyYlGRsnpKcYpRqmZyWaGlmsuKydkpDICPD5Co+VkYGCATxWRmSc/JLUxgYAAfyIYM=',
+  agoraChannel: 'cloud',
 
-  // map 
+  // map
   // You can apply on the AMap website.
-  amapKey: 'Please enter the amap key.',
-
+  amapKey: '1b517bc082f2b671fffe6391ea727751',
+  AMAP_KEY: '8410cfe5bcb83f686d0dbeafffc18fcf'
 }
